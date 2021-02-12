838896c6b9a89ada80ed640964dc768c335b051d
Regis Ribeiro <regisgomesr@gmail.com>
2021-02-12 18:51:00 -0300

    Componente Lista de repositorios do github e redux no typescript

---
+++ src/store/ducks/repositories/actions.ts
@@ -0,0 +1,13 @@
+/**
+ * Actions = São Funções que disparam nossos types
+ */
+
+import { action } from "typesafe-actions";
+import { RepositoriesTypes, Repository } from "./types";
+
+export const loadRequest = () => action(RepositoriesTypes.LOAD_REQUEST);
+
+export const loadSuccess = (data: Repository[]) =>
+  action(RepositoriesTypes.LOAD_SUCCESS, data);
+
+export const loadFailure = () => action(RepositoriesTypes.LOAD_FAILURE);
